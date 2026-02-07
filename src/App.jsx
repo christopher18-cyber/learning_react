a6448f33a8886369529f549f4967b440753d5f17
christopher18-cyber <christopheradegoke4@gmail.com>
2026-02-07 09:44:03 +0100

    started learning react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ClassBasedComponent from './components/class-based-component.jsx'
+import FunctionalComponents from './components/functional-based-component.jsx'
+
+function App() {
+
+  return (
+    <div>
+      <h1>React concepts</h1>
+      <ClassBasedComponent />
+      <FunctionalComponents />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/functional-based-component.jsx
@@ -0,0 +1,17 @@
+// function FunctionalComponents() {
+//     return (
+//         <div>
+//             <h4>Functional component</h4>
+//         </div>
+//     );
+// }
+
+const FunctionalComponents = () => {
+    return (
+        <div>
+            <h4>Functional component</h4>
+        </div>
+    )
+}
+
+export default FunctionalComponents
